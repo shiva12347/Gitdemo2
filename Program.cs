db3a89895ac33bcb6220757589c2e3340d2723f7
shivakumar <sk625700@gmail.com>
2021-11-10 17:15:51 +0530

    new changes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Gitdemo2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello SHIVA ");
         }
     }
 }
